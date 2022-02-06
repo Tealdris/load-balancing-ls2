6c47807cd1669764dffd0febfe3be02200d0a722
Tealdris <ilyaradomskiy7771@gmail.com>
2022-02-06 16:01:52 +0300

    lol

--- app2/app2.py
+++ app2/app2.py
@@ -7,7 +7,7 @@ app1 = Flask(__name__)
 
 @app1.route('/')
 def hello_world():
-    return ('Hello to SNE family! Time is: stop ot')
+    return ('Hello to SNE family! Time is:', 'lol')
 
 
 if __name__ == '__main__':
